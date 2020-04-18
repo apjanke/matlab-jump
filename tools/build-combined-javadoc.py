749b92d4a9055fc008cc8adcae26f5a517378295
Andrew Janke <andrew@apjanke.net>
2020-04-18 19:13:09 -0400

    Create an index file for the javadocs

--- tools/build-combined-javadoc.py
+++ tools/build-combined-javadoc.py
@@ -9,10 +9,22 @@ def maybeMakedirs(dir):
 
 jarDir = "opp/javadoc"
 htmlDir = "javadocs"
+indexFile = htmlDir + "/index.html"
 
 maybeMakedirs(htmlDir)
 
+fIndex = open(indexFile, 'w')
+indexHead = """<html>
+<body>
+
+<h2>Java Libraries shipped with Matlab R2019b</h2>
+
+<ul>
+"""
+fIndex.write(indexHead)
+
 jars = glob.glob(jarDir + "/*.jar")
+jars.sort()
 for jar in jars:
   file = os.path.basename(jar)
   base = os.path.splitext(file)
@@ -24,3 +36,13 @@ for jar in jars:
   os.chdir(htmlSubdir)
   subprocess.run(["jar", "xf", "../../opp/javadoc/"+file ])
   os.chdir("../..")
+  fIndex.write(f'  <li><a href="{lib}/index.html">{lib}</a></li>\n')
+
+indexTail = """
+</ul>
+
+</body>
+</html>
+"""
+fIndex.close()
+
